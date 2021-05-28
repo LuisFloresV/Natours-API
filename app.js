dab894f6f93c0571d1887b1d91c07e19e713f026
Luis Daniel Flores Velazquez <luisfv98@outlook.com>
2021-05-28 16:15:43 -0500

    Error class

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const morgan = require('morgan')
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
+const AppError = require('./utils/appError')
+const globalErrorHandler = require('./controllers/errorController')
 
 const app = express()
 
@@ -16,10 +18,9 @@ app.use('/api/v1/users', userRouter)
 
 //404 Middleware
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl}`
-  })
+  next(new AppError(`Can't find ${req.originalUrl}`, 404))
 })
+
+app.use(globalErrorHandler)
 
 module.exports = app
