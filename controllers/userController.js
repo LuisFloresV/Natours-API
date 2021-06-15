f5b2fd04506a175a2bb284464e9614d364111da6
Luis Daniel Flores Velazquez <luisfv98@outlook.com>
2021-06-15 16:26:59 -0500

    Added get me route

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,3 +41,8 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
     data: null,
   })
 })
+
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id
+  next()
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,18 +5,18 @@ const {
   signUp, login, forgotPassword, resetPassword, protect, updatePassword, restrictTo,
 } = require('../controllers/authController')
 const {
-  createUser, deleteUser, getAllUsers, getUser, updateUser, updateMe, deleteMe,
+  createUser, deleteUser, getAllUsers, getUser, updateUser, updateMe, deleteMe, getMe,
 } = require('../controllers/userController')
 
 // Authentication
 router.post('/signup', signUp)
 router.post('/login', login)
-
 router.post('/forgotPassword', forgotPassword)
 router.patch('/resetPassword/:token', resetPassword)
 router.patch('/updateMyPassword', protect, updatePassword)
 router.patch('/updateMe', protect, updateMe)
 router.delete('/deleteMe', protect, deleteMe)
+router.get('/me', protect, getMe, getUser)
 
 router
   .route('/')
