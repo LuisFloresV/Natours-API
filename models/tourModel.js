0177483465e4000cf732e859ad2beb169b333467
Luis Daniel Flores Velazquez <luisfv98@outlook.com>
2021-06-15 17:54:35 -0500

    Index for price, ratingsAverage, and slug

--- models/tourModel.js
+++ models/tourModel.js
@@ -115,6 +115,10 @@ const tourSchema = new mongoose.Schema({
   toObject: { virtuals: true },
 })
 
+// tourSchema.index({ price: 1 })
+tourSchema.index({ price: 1, ratingsAverage: -1 })
+tourSchema.index({ slug: 1 })
+
 // Not in the DB
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7
